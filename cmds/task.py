29af47de6792b24204b5b4fca21376c6984032ff
lo2003058 <lo2003058@github.com>
2019-09-05 17:27:21 +0800

    Update task.py

--- cmds/task.py
+++ cmds/task.py
@@ -10,14 +10,15 @@ class Task(Cog_Extension):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        async def interval():
-            await self.bot.wait_until_ready()
-            self.channel = self.bot.get_channel(jdata['test_channel'])
-            while not self.bot.is_closed():
-                await self.channel.send('test')
-                await asyncio.sleep(5)
 
-        self.bg_task = self.bot.loop.create_task(interval())
+        # async def interval():
+        #     await self.bot.wait_until_ready()
+        #     self.channel = self.bot.get_channel(jdata['test_channel'])
+        #     while not self.bot.is_closed():
+        #         await self.channel.send('test')
+        #         await asyncio.sleep(5)
+        #
+        # self.bg_task = self.bot.loop.create_task(interval())
 
 
 def setup(bot):
